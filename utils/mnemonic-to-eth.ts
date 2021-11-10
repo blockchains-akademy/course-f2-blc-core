3b8da4e5786fd13d330b0bd902929b3b3f5b33d3
demo <phanthanh.trang200@gmail.com>
2021-11-10 13:33:04 +0700

    convert address from Metamask public key to Substrate address

---
+++ utils/mnemonic-to-eth.ts
@@ -0,0 +1,36 @@
+const bip39 = require('bip39')
+const hdkey = require('hdkey')
+const bip44Constants = require('bip44-constants')
+
+function getCoinType(blockchain) {
+  return bip44Constants[blockchain] - 0x80000000
+}
+
+function getKey(root, coinType) {
+  const addrNode = root.derive(`m/44'/${coinType}'/0'/0/0`)
+  return {
+    address: 0, //addrNode,
+    ethKey: addrNode._privateKey.toString('hex')
+  }
+}
+
+function printPrivateKeys(_mnemonic = null) {
+  const mnemonic = _mnemonic || bip39.generateMnemonic()
+  if (!bip39.validateMnemonic(mnemonic)) {
+    throw new Error('Invalid mnemonic!')
+  }
+  const seed = bip39.mnemonicToSeed(mnemonic)
+  const root = hdkey.fromMasterSeed(seed)
+
+  const ethKey = getKey(root, getCoinType('ETH'))
+
+  const res = {
+    Address: ethKey.address,
+    ETH: `0x${ethKey.ethKey}`,
+    mnemonic,
+  }
+
+  return res
+}
+
+console.info(printPrivateKeys(process.argv[2]))
